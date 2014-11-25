8235dbd037a2dc4778e7433af4abe9f8498c6fce
Zack Weinberg <zackw@panix.com>
2014-11-24 20:21:12 -0500

    Restore blogroll.

--- pelican/conf.py
+++ pelican/conf.py
@@ -64,14 +64,148 @@ MENUITEMS = [
 RECENT_POSTS_LENGTH = 5
 
 # Blogroll
-#LINKS = (('Pelican', 'http://getpelican.com/'),
-#         ('Python.org', 'http://python.org/'),
-#         ('Jinja2', 'http://jinja.pocoo.org/'),
-#         ('You can modify those links in your config file', '#'),)
+BLOGROLL = [
+    ("Tribe", '', [
+        ("Pam",
+         "Pamela Griffith ♥",
+         "http://www.pamgriffith.net/"),
 
-# Social widget
-#SOCIAL = (('You can add links in your config file', '#'),
-#          ('Another social link', '#'),)
+        ("Dara",
+         "Dara Weinberg",
+         "http://darastrata.com/"),
+
+        ("Adam",
+         "Adam Burr",
+         "https://twitter.com/madmanatw"),
+
+        ("Brendan",
+         "Brendan Adkins",
+         "http://www.xorph.com/nfd/"),
+
+        ("Caroline",
+         "Caroline Ratajski",
+         "http://www.geardrops.net/"),
+
+        ("Janna",
+         None,
+         "http://readingwatchinglookingandstuff.blogspot.com/"),
+
+        ("Jo",
+         "Jo Walton",
+         "http://papersky.livejournal.com/"),
+
+        ("Jon",
+         "Jon Evans",
+         "http://www.rezendi.com/"),
+
+        ("Kashy",
+         "Kashmiri Stec",
+         "http://lutin.livejournal.com/profile"),
+
+        ("Leonard",
+         "Leonard Richardson",
+         "http://www.crummy.com/"),
+
+        ("Mris",
+         "Marissa Lingen",
+         "http://marissalingen.com/"),
+
+        ("Rysmiel",
+         None,
+         "http://rysmiel.livejournal.com/profile"),
+
+        ("Shweta",
+         "Shweta Narayan",
+         "http://shwetanarayan.org/"),
+
+        ("Sumana",
+         "Sumana Harihareswara",
+         "http://www.harihareswara.net/"),
+
+        ("Turadg",
+         "Turadg Aleahmad",
+         "http://turadg.aleahmad.net/"),
+    ]),
+    ("Affiliated", '', [
+        ("Tor",
+         "Tor Project (anonymity online), 2012–",
+         "https://www.torproject.org"),
+
+        ("CMU/ECE",
+         "Computer security PhD program at CMU, 2010–",
+         "https://www.ece.cmu.edu/"),
+
+        ("Mozilla",
+         "Mozilla Project, 2008–",
+         "https://www.mozilla.org/"),
+
+        ("monotone",
+         "Monotone version control system, 2007–2009",
+         "http://monotone.ca/"),
+
+        ("GCC",
+         "GNU Compiler Collection, 1998–2006 (plus a couple of relapses)",
+         "https://gcc.gnu.org/"),
+
+        ("UCSD",
+         "Cognitive Science department at UC San Diego, 2006–2008",
+         "http://www.cogsci.ucsd.edu/"),
+
+        ("NTL",
+         "Neural Theory of Language group at UC Berkeley, 2001–2006",
+         "https://ntl.icsi.berkeley.edu/ntl/"),
+
+        ("CodeSourcery",
+         "Bespoke GCC development and support, 2001–2006",
+         "http://www.codesourcery.com/"),
+
+        ("Rabi",
+         "I.I. Rabi Science Scholars Program at Columbia, 1995–1999",
+         "https://www.college.columbia.edu/academics/rabi"),
+
+        ("CUMB",
+         "The Cleverest Band in the World, 1995–1999",
+         "http://cuband.org/"),
+
+        ("SSP",
+         "Summer Science Program (1994)",
+         "http://www.summerscience.org/"),
+    ]),
+
+    ("Also Me", 'rel="me"', [
+        ("Twitter",
+         "Chitchat",
+         "https://twitter.com/elwoz/"),
+
+        ("Newsblur",
+         "Link scrapbooking",
+         "https://zwol.newsblur.com/"),
+
+        ("Stack Overflow",
+         "The dusty corners of C",
+         "https://stackoverflow.com/users/388520/zack"),
+
+        ("Github",
+         "Code",
+         "https://github.com/zackw/"),
+
+        ("Flickr",
+         "Architecture, trees, and relatives",
+         "http://flickr.com/photos/zackw/"),
+
+        ("Google+",
+         "Longer chitchat and Breakfast Combo",
+         "https://plus.google.com/108735008363901796353/about"),
+
+        ("Dreamwidth",
+         "Old personal journal, no longer updated",
+         "https://zwol.dreamwidth.org/"),
+
+        ("LinkedIn",
+         "I AM NOT LOOKING FOR A JOB. DO NOT SEND ME JOB OFFERS.",
+         "https://www.linkedin.com/in/zackweinberg/"),
+    ])
+]
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -79,4 +213,3 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
-
