85c724ad1b3f4c439ee521d5d59ea99469c1fe36
Zack Weinberg <zackw@panix.com>
2021-01-19 16:57:45 -0500

    Fixes only affecting ‘make publish’.

--- pelican/conf_pub.py
+++ pelican/conf_pub.py
@@ -8,18 +8,23 @@
 # pelican directory on sys.path permanently.
 import os.path
 from pelican.settings import load_source
-load_source("conf_base",
-            os.path.join(os.path.dirname(__file__),
-                         "conf.py"))
-from conf_base import *
+gzip_cache = load_source("gzip_cache",
+                         os.path.join(os.path.dirname(__file__),
+                                      "local-plugins/gzip_cache.py"))
+conf_base = load_source("conf_base",
+                        os.path.join(os.path.dirname(__file__),
+                                     "conf.py"))
+for setting in dir(conf_base):
+    globals()[setting] = getattr(conf_base, setting)
+del setting, conf_base
 
 SITEURL            = 'https://www.owlfolio.org'
 FEED_DOMAIN        = SITEURL
 FEED_MAX_ITEMS     = DEFAULT_PAGINATION
 FEED_ALL_ATOM      = 'index.atom'
-CATEGORY_FEED_ATOM = '%s/index.atom'
+CATEGORY_FEED_ATOM = '{slug}/index.atom'
 
-PLUGINS.extend(['gzip_cache'])
+PLUGINS.extend([gzip_cache])
 STATIC_PATHS.append('../meta')
 ARTICLE_EXCLUDES.append('../meta')
 
