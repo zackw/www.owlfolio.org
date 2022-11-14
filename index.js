a5e6beb0f4f080c6c6c45d3675a8b5f4f7d772b2
Zack Weinberg <zackw@panix.com>
2022-11-14 09:02:37 -0500

    Add development-server and linting logic.
    Cribbed from https://github.com/wernerglinka/metalsmith-blog-starter:
    
     - eslint integration
     - autoindenting using ‘prettier’
     - npm scripts to run a local web server for development

--- index.js
+++ index.js
@@ -13,35 +13,27 @@ const md_math = require("markdown-it-katex");
 
 const hljs = require("highlight.js");
 
-function hljs_or_escape(str, lang) {
+const markdown = MarkdownIt({
+  typographer: true,
+  html: true,
+  highlight: (str, lang) => {
     if (lang) {
-        const hlang = hljs.getLanguage(lang);
-        if (hlang) {
-            const canon_lang = hlang.name.toLowerCase();
-            try {
-                const highlighted = hljs.highlight(str, {
-                    language: lang,
-                }).value;
-                return `<pre class="hljs language-${canon_lang}"><code>`
-                    + highlighted
-                    + '</code></pre>';
-            } catch (__) {
-            }
-        }
+      const hlang = hljs.getLanguage(lang);
+      if (hlang) {
+        const canon_lang = hlang.name.toLowerCase();
+        try {
+          const highlighted = hljs.highlight(str, {
+            language: lang,
+          }).value;
+          return `<pre class="hljs language-${canon_lang}"><code>${highlighted}</code></pre>`;
+        } catch (_unused) {}
+      }
     }
     // if we get here, either we don't have a language tag or it wasn't
     // recognized
-
-    return '<pre class="hljs language-none"><code>'
-        + markdown.parser.utils.escapeHtml(str)
-        + '</code></pre>';
-}
-
-
-const markdown = MarkdownIt({
-    typographer: true,
-    html: true,
-    highlight: hljs_or_escape,
+    const highlighted = markdown.parser.utils.escapeHtml(str);
+    return `<pre class="hljs language-none"><code>${highlighted}</code></pre>`;
+  },
 });
 markdown.parser.use(md_deflist);
 markdown.parser.use(md_attrs);
@@ -52,39 +44,45 @@ markdown.parser.use(md_sub);
 markdown.parser.use(md_math);
 
 function debug_tree(tag) {
-    return function (ms) {
-        for (let k of Object.keys(ms).sort()) {
-            console.log(tag, k);
-        }
-    };
+  return (ms) => {
+    for (const k of Object.keys(ms).sort()) {
+      console.log(tag, k);
+    }
+  };
 }
 
 Metalsmith(__dirname)
-    .metadata({
-        SITENAME: "Owl’s Portfolio",
-        AUTHOR: 'Zack Weinberg',
-        TIMEZONE: 'America/New_York',
-        DEFAULT_LANG: 'en',
-        DEFAULT_PAGINATION: 10,
+  .metadata({
+    SITENAME: "Owl’s Portfolio",
+    AUTHOR: "Zack Weinberg",
+    TIMEZONE: "America/New_York",
+    DEFAULT_LANG: "en",
+    DEFAULT_PAGINATION: 10,
+  })
+  .source("src")
+  .destination("build")
+  .ignore("comments") // for now
+  .ignore("meta") // for now
+  .use(markdown)
+  .use(
+    permalinks({
+      duplicatesFail: true,
+      // unique: true,
+      relative: false,
     })
-    .source('src')
-    .destination('build')
-    .ignore('comments') // for now
-    .ignore('meta') // for now
-    .use(markdown)
-    .use(debug_tree("before_permalinks:"))
-    .use(permalinks({
-        duplicatesFail: true,
-        //unique: true,
-        relative: false,
-    }))
-    .use(debug_tree("after_permalinks:"))
-    .use(layouts({
-        pattern: 'pages/**/index.html',
-        default: 'page.njk',
-    }))
-    .use(layouts({
-        pattern: 'posts/**/index.html',
-        default: 'post.njk',
-    }))
-    .build(function (err, files) { if (err) throw err; });
+  )
+  .use(
+    layouts({
+      pattern: "pages/**/index.html",
+      default: "page.njk",
+    })
+  )
+  .use(
+    layouts({
+      pattern: "posts/**/index.html",
+      default: "post.njk",
+    })
+  )
+  .build((err, _unused) => {
+    if (err) throw err;
+  });
