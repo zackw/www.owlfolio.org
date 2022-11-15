7561104a6b3e72c411e991a3d3a0405ab2235b2e
Zack Weinberg <zackw@panix.com>
2022-11-15 14:43:48 -0500

    Fix up the top navigation bar and the outbound links.
    Mostly a matter of tweaking the templates for differences between
    Jinja and Nunjucks.  Also the data itself had to be re-imported.

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import Metalsmith from "metalsmith";
 import default_values from "@metalsmith/default-values";
 import layouts from "@metalsmith/layouts";
+import metadata from "@metalsmith/metadata";
 import permalinks from "@metalsmith/permalinks";
+
+import beautify from "metalsmith-beautify";
+import cond from "metalsmith-if";
 
 import * as url from "url";
 import { custom_markdown, rename_patterns } from "./lib/local-plugins.js";
@@ -16,17 +20,25 @@ import { custom_markdown, rename_patterns } from "./lib/local-plugins.js";
 
 async function main() {
   const source_root = url.fileURLToPath(new URL(".", import.meta.url));
+  const mode = process.env.NODE_ENV || "development";
   await Metalsmith(source_root)
     .metadata({
-      SITENAME: "Owl’s Portfolio",
-      AUTHOR: "Zack Weinberg",
-      TIMEZONE: "America/New_York",
-      DEFAULT_LANG: "en",
-      DEFAULT_PAGINATION: 10,
+      sitename: "Owl’s Portfolio",
     })
     .source("src")
     .destination("build")
-    .ignore("comments") // until we have something better to do with them
+    .ignore([
+      ".#*", // emacs detritus
+      "#*#",
+      "*~",
+      ".*.sw[a-p]", // vim detritus
+      "comments", // until we have something better to do with them
+    ])
+    .use(
+      metadata({
+        out_links: "src/meta/outlinks.yaml",
+      })
+    )
     .use(await custom_markdown())
     .use(
       permalinks({
@@ -49,8 +61,15 @@ async function main() {
     )
     .use(layouts())
     .use(
+      cond(
+        mode === "development",
+        beautify({ indent_size: 2, indent_char: " " })
+      )
+    )
+    .use(
       rename_patterns([
         { file: "meta/htaccess", to: ".htaccess" },
+        { file: "meta/cc-by-nc-4-80x15.png", to: "s/cc-by-nc-4-80x15.png" },
         { file: "pages/404/index.html", to: "s/404.html" },
         {
           file: ["meta/**", "pages/**", "posts/**"],
@@ -64,5 +83,5 @@ async function main() {
 main().then(() => {});
 
 // Local Variables:
-// js2-additional-externs: ("URL")
+// js2-additional-externs: ("URL", "process")
 // End:
