27ee41e52ed686fa25bfc92d8948e4130928536c
Zack Weinberg <zackw@panix.com>
2022-11-15 12:03:43 -0500

    Better way of handling file renames.
    Generalize the ‘rename_to_prop’ local plugin to the point where it can
    handle the work being done by move_up as well.  The configuration
    migrates from file frontmatter to index.js (it will, perhaps, move to
    a separate config file later) and this means we no longer need the
    metafiles plugin either.
    
    There is now enough code in local plugins to warrant splitting them
    out of index.js.

--- index.js
+++ index.js
@@ -2,13 +2,9 @@ import Metalsmith from "metalsmith";
 import default_values from "@metalsmith/default-values";
 import layouts from "@metalsmith/layouts";
 import permalinks from "@metalsmith/permalinks";
-import markdown_it from "metalsmith-markdownit";
-import metafiles from "metalsmith-metafiles";
-import move_up from "metalsmith-move-up";
-
-import hljs from "highlight.js";
 
 import * as url from "url";
+import { custom_markdown, rename_patterns } from "./lib/local-plugins.js";
 
 // function debug_tree(tag) {
 //   return (ms) => {
@@ -17,66 +13,6 @@ import * as url from "url";
 //     }
 //   };
 // }
-
-async function custom_markdown() {
-  const markdown = markdown_it({
-    typographer: true,
-    html: true,
-    highlight: (str, lang) => {
-      if (lang) {
-        const hlang = hljs.getLanguage(lang);
-        if (hlang) {
-          const canon_lang = hlang.name.toLowerCase();
-          try {
-            const highlighted = hljs.highlight(str, {
-              language: lang,
-            }).value;
-            return `<pre class="hljs language-${canon_lang}"><code>${highlighted}</code></pre>`;
-          } catch (_unused) {}
-        }
-      }
-      // if we get here, either we don't have a language tag or it wasn't
-      // recognized
-      const highlighted = markdown.parser.utils.escapeHtml(str);
-      return `<pre class="hljs language-none"><code>${highlighted}</code></pre>`;
-    },
-  });
-
-  const parser_extensions = [
-    "anchor",
-    "attrs",
-    "deflist",
-    "footnote",
-    "katex",
-    "sub",
-    "sup",
-  ];
-  const parser_mods = await Promise.all(
-    parser_extensions.map((mdx) => import(`markdown-it-${mdx}`))
-  );
-  parser_mods.map((mod) => markdown.parser.use(mod.default));
-
-  return markdown;
-}
-
-/// Mini-plugin that looks for a file property PROP, and if it's found,
-/// changes the name of that file to the value of the property
-function rename_to_prop(prop) {
-  return (files, _unused, done) => {
-    Object.keys(files).forEach((file) => {
-      const blob = files[file];
-      if (Object.hasOwn(blob, prop)) {
-        const renamed = blob[prop];
-        if (Object.hasOwn(files, renamed)) {
-          throw new Error(`collision: ${file} -> ${renamed} already present`);
-        }
-        files[renamed] = blob;
-        delete files[file];
-      }
-    });
-    done();
-  };
-}
 
 async function main() {
   const source_root = url.fileURLToPath(new URL(".", import.meta.url));
@@ -91,11 +27,6 @@ async function main() {
     .source("src")
     .destination("build")
     .ignore("comments") // until we have something better to do with them
-    .use(
-      metafiles({
-        postfix: ".m",
-      })
-    )
     .use(await custom_markdown())
     .use(
       permalinks({
@@ -117,8 +48,17 @@ async function main() {
       ])
     )
     .use(layouts())
-    .use(move_up(["pages/**", "posts/**"]))
-    .use(rename_to_prop("save_as"))
+    .use(
+      rename_patterns([
+        { file: "meta/htaccess", to: ".htaccess" },
+        { file: "pages/404/index.html", to: "s/404.html" },
+        {
+          file: ["meta/**", "pages/**", "posts/**"],
+          from: /^[^/]+\//,
+          to: "",
+        },
+      ])
+    )
     .build();
 }
 main().then(() => {});
