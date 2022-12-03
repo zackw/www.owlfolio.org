60c3d98cf088ebd140579e18f157a89871492afa
Zack Weinberg <zackw@panix.com>
2022-12-03 11:39:41 -0500

    Stop chaining metalsmith.use().
    It is easier to restructure the pipeline if each .use call is an
    independent statement.
    
    The diff is longer than ideal due to indentation changes;
    semantically, the change is s/ \)\s*\.use\( / ); ms.use( /x.

--- index.js
+++ index.js
@@ -105,118 +105,118 @@ async function main() {
       metadata({
         out_links: "src/meta/outlinks.yaml",
       })
-    )
-    .use(
-      edit_history({
-        patterns: ["posts/*/*.{html,md}", "!posts/*/index.{html,md}"],
-        exclude: [
-          // These are all the commits that touched files in posts/ to
-          // make changes that don't, or only trivially, affect content.
-          "066205e57f4b9abbcc447a35268a32d4a2027c3f",
-          "116522beda01270146e68822dc17388c14a35498",
-          "1495b48d3d152a4081385a6c5bef475d5a451dd1",
-          "19547448b1fae06e1a43b2660a4fb6b5fb22acfe",
-          "3cad6bb3b378f1a122632bb2cfa39cb96cdacaec",
-          "690d3f1f15ef50c5bf3bc1e8a4bf7239f8d259db",
-          "6aed9536008603405f57b4d979002ea0a1742751",
-          "6e8860a469ac1f0abc69da46ca19182f3ed3700a",
-          "811a6f0e9109d359fb5ca524cfce0269366ae187",
-          "873d2d676b1467dfb2575752ba5b4edb218aa1e2",
-          "93d8a565a459d55549048f7a0d1b3d328a774069",
-          "b0d27ac3b8553a6f7d92367a53c5deadd24d7cc2",
-          "bbd8a0a65c674015006f86e995a82a3eb24335b6",
-          "bc97ff6de477d2f19f30e183004057cdbadf592b",
-          "cf74c9a57bec2cb69dc8b5fdaa72a263bcb13e61",
-          "d492493a2937d434332c61410650064ebdb7fb99",
-          "d915b45880fe6c3199114b0ce1a8335773cb630f",
-          "dba4f7e3abfb3373c3b06730965ec1128109923f",
-          "ea3fd9fabb19afece0b04b2772fba0a20cbb2cf8",
-          "ec5628aa86b17de5ec1d0d53e5d5b7f8e58bf0b2",
-          "f26985732d4b7af44dccdfd167bce030d4c779fd",
-          "f355fbb6d5e8490f470b61163590ec964297d142",
-          "f8e48eadeae1de531a8b30e2ab62048a35fc10ea",
-          "ff8cb06f05683a2a07e654ccb30fb0ebd7f76e3f",
-        ],
-        date_opts: { zone: "America/New_York" },
-      })
-    )
-    .use(
-      defaults_from_path([
-        {
-          pattern: "posts/*/*.{html,md}",
-          defaults: (_unused1, path, _unused2) => {
-            const [, category, post] = path.match(
-              /^posts\/([^/]+)\/([^/]+)\.(?:html|md)$/u
-            );
-            if (post === "index") {
-              return {
-                collection: category,
-                layout: "category.njk",
-                permalink: false,
-              };
-            } else {
-              return {
-                collection: category,
-                layout: "post.njk",
-              };
-            }
-          },
+    );
+  ms.use(
+    edit_history({
+      patterns: ["posts/*/*.{html,md}", "!posts/*/index.{html,md}"],
+      exclude: [
+        // These are all the commits that touched files in posts/ to
+        // make changes that don't, or only trivially, affect content.
+        "066205e57f4b9abbcc447a35268a32d4a2027c3f",
+        "116522beda01270146e68822dc17388c14a35498",
+        "1495b48d3d152a4081385a6c5bef475d5a451dd1",
+        "19547448b1fae06e1a43b2660a4fb6b5fb22acfe",
+        "3cad6bb3b378f1a122632bb2cfa39cb96cdacaec",
+        "690d3f1f15ef50c5bf3bc1e8a4bf7239f8d259db",
+        "6aed9536008603405f57b4d979002ea0a1742751",
+        "6e8860a469ac1f0abc69da46ca19182f3ed3700a",
+        "811a6f0e9109d359fb5ca524cfce0269366ae187",
+        "873d2d676b1467dfb2575752ba5b4edb218aa1e2",
+        "93d8a565a459d55549048f7a0d1b3d328a774069",
+        "b0d27ac3b8553a6f7d92367a53c5deadd24d7cc2",
+        "bbd8a0a65c674015006f86e995a82a3eb24335b6",
+        "bc97ff6de477d2f19f30e183004057cdbadf592b",
+        "cf74c9a57bec2cb69dc8b5fdaa72a263bcb13e61",
+        "d492493a2937d434332c61410650064ebdb7fb99",
+        "d915b45880fe6c3199114b0ce1a8335773cb630f",
+        "dba4f7e3abfb3373c3b06730965ec1128109923f",
+        "ea3fd9fabb19afece0b04b2772fba0a20cbb2cf8",
+        "ec5628aa86b17de5ec1d0d53e5d5b7f8e58bf0b2",
+        "f26985732d4b7af44dccdfd167bce030d4c779fd",
+        "f355fbb6d5e8490f470b61163590ec964297d142",
+        "f8e48eadeae1de531a8b30e2ab62048a35fc10ea",
+        "ff8cb06f05683a2a07e654ccb30fb0ebd7f76e3f",
+      ],
+      date_opts: { zone: "America/New_York" },
+    })
+  );
+  ms.use(
+    defaults_from_path([
+      {
+        pattern: "posts/*/*.{html,md}",
+        defaults: (_unused1, path, _unused2) => {
+          const [, category, post] = path.match(
+            /^posts\/([^/]+)\/([^/]+)\.(?:html|md)$/u
+          );
+          if (post === "index") {
+            return {
+              collection: category,
+              layout: "category.njk",
+              permalink: false,
+            };
+          } else {
+            return {
+              collection: category,
+              layout: "post.njk",
+            };
+          }
         },
-        {
-          pattern: "pages/*.{html,md}",
-          defaults: { layout: "page.njk" },
-        },
-      ])
-    )
-    .use(
-      collections({
-        posts: {
-          pattern: "posts/*/*.{html,md}",
-          refer: true,
-        },
-        pages: {
-          pattern: "pages/*.md",
-          filterBy: (file) => !Object.hasOwn(file, "save_as"),
-        },
-      })
-    )
-    .use(await custom_markdown())
-    .use(
-      teasers({
-        pattern: ["posts/*/*.html", "!posts/*/index.html"],
-        teaser_key: "excerpt",
-      })
-    )
-    .use(
-      permalinks({
-        duplicatesFail: true,
-        relative: false,
-      })
-    )
-    .use(
-      rename_patterns([
-        { file: "meta/htaccess", to: ".htaccess" },
-        { file: "meta/cc-by-nc-4-80x15.png", to: "s/cc-by-nc-4-80x15.png" },
-        { file: "pages/404/index.html", to: "s/404.html" },
-        {
-          file: ["meta/**", "pages/**", "posts/**"],
-          from: /^[^/]+\//u,
-          to: "",
-        },
-      ])
-    )
-    .use(
-      pagination({
-        "collections.posts": {
-          perPage: 10,
-          layout: "archive.njk",
-          first: "index.html",
-          noPageOne: true,
-          path: ":num/index.html",
-        },
-      })
-    )
-    .use(custom_nunjucks());
+      },
+      {
+        pattern: "pages/*.{html,md}",
+        defaults: { layout: "page.njk" },
+      },
+    ])
+  );
+  ms.use(
+    collections({
+      posts: {
+        pattern: "posts/*/*.{html,md}",
+        refer: true,
+      },
+      pages: {
+        pattern: "pages/*.md",
+        filterBy: (file) => !Object.hasOwn(file, "save_as"),
+      },
+    })
+  );
+  ms.use(await custom_markdown());
+  ms.use(
+    teasers({
+      pattern: ["posts/*/*.html", "!posts/*/index.html"],
+      teaser_key: "excerpt",
+    })
+  );
+  ms.use(
+    permalinks({
+      duplicatesFail: true,
+      relative: false,
+    })
+  );
+  ms.use(
+    rename_patterns([
+      { file: "meta/htaccess", to: ".htaccess" },
+      { file: "meta/cc-by-nc-4-80x15.png", to: "s/cc-by-nc-4-80x15.png" },
+      { file: "pages/404/index.html", to: "s/404.html" },
+      {
+        file: ["meta/**", "pages/**", "posts/**"],
+        from: /^[^/]+\//u,
+        to: "",
+      },
+    ])
+  );
+  ms.use(
+    pagination({
+      "collections.posts": {
+        perPage: 10,
+        layout: "archive.njk",
+        first: "index.html",
+        noPageOne: true,
+        path: ":num/index.html",
+      },
+    })
+  );
+  ms.use(custom_nunjucks());
 
   await use_postprocess_plugins(ms, is_development);
   await ms.build();
